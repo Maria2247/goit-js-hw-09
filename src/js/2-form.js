b23af5b8202b0fc45d9e0174f21b76c82131405d
Maria2247 <mashasavchenko@yahoo.com>
2024-04-28 22:51:59 +0300

    variant1

---
+++ src/js/2-form.js
@@ -0,0 +1,43 @@
+const formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+
+const localStorageKey = 'feedback-form-state';
+
+form.addEventListener('input', event => {
+  formData.email = event.currentTarget.email.value;
+  formData.message = event.currentTarget.message.value;
+
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+});
+
+function populateStoredForm() {
+  const savedFormData = localStorage.getItem(localStorageKey);
+
+  if (savedFormData) {
+    const parsedFormData = JSON.parse(savedFormData);
+    const parsedEmail = parsedFormData.email;
+    const parsedMessage = parsedFormData.message;
+
+    form.elements.email.value = parsedEmail;
+    form.elements.message.value = parsedMessage;
+  }
+}
+
+populateStoredForm();
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const emailValue = event.currentTarget.email.value;
+  const messageValue = event.currentTarget.message.value;
+  if (emailValue === '' || messageValue === '') {
+    alert('Fill please all fields');
+  }
+  localStorage.removeItem(localStorageKey);
+  event.currentTarget.reset();
+}
+
+form.addEventListener('submit', onFormSubmit);
